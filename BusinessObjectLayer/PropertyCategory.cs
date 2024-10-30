c29df3b548bed954acd066c4bc72a94397920328
Pham Hoang Minh Khoi <phamkhoi172003@gmail.com>
2024-10-30 13:55:23 +0700

    Startup project config

---
+++ BusinessObjectLayer/PropertyCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjectLayer;
+
+public partial class PropertyCategory
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public virtual ICollection<Property> Properties { get; set; } = new List<Property>();
+
+    public override string ToString()
+    {
+        return CategoryName;
+    }
+}
---
+++ DataAccessLayer/PropertyDAO.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjectLayer;
+
+namespace DataAccessLayer
+{
+    public class PropertyDAO
+    {
+        private readonly SaleManagementContext _context;
+        public static PropertyDAO instance = null;
+        public static PropertyDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new PropertyDAO();
+                }
+                return instance;
+            }
+        }
+
+        public PropertyDAO()
+        {
+            _context = new SaleManagementContext();
+        }
+
+        public void AddProperty(Property property)
+        {
+            _context.Properties.Add(property);
+            _context.SaveChanges();
+        }
+
+        public Property GetPropertyById(int propertyId)
+        {
+            return _context.Properties
+                           .Include(p => p.Category)
+                           .FirstOrDefault(p => p.PropertyId == propertyId);
+        }
+
+        public List<Property> GetAllProperties()
+        {
+            return _context.Properties
+                           .Include(p => p.Category)
+                           .ToList();
+        }
+
+        public void UpdateProperty(Property property)
+        {
+            var existingProperty = _context.Properties.Find(property.PropertyId);
+            if (existingProperty != null)
+            {
+                _context.Entry(existingProperty).CurrentValues.SetValues(property);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteProperty(int propertyId)
+        {
+            var property = _context.Properties.Find(propertyId);
+            if (property != null)
+            {
+                _context.Properties.Remove(property);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
